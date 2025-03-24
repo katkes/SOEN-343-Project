9ae2fabdcea7491d39a42a41c981773584ac7cf7
Mouhamed Kairson Coundoul <mcoundoul6@gmail.com>
2025-03-24 18:12:13 -0400

    Enhance styling by making tab font bold and updating Badge component to use bold font for labels

--- frontend/src/components/Badge.tsx
+++ frontend/src/components/Badge.tsx
@@ -1,7 +1,7 @@
 export default function Badge({ label, className }: { label: string; className?: string }) {
     return (
       <div className={`badge ${className ? className : ''}`}>
-        <div className="font-medium text-[#273266]">{label}</div>
+        <div className="font-bold text-[#273266]">{label}</div>
       </div>
     );
   }
