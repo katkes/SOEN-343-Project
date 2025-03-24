ac2d6e4754001c7970e3827f77cbab2f5331e1d7
Mouhamed Kairson Coundoul <mcoundoul6@gmail.com>
2025-03-24 18:01:16 -0400

    Enhance styling for login and signup forms, refactor Hero component to accept servicesRef prop, and update NavBar import path

--- frontend/src/pages/home/Hero.tsx
+++ frontend/src/pages/home/Hero.tsx
@@ -3,30 +3,38 @@ import CustomButton from '../../components/CustomButton';
 import { Link } from 'react-router';
 import { FrontEndRoutes } from '../routes';
 
-export default function Hero() {
-  return (
-    <>
-      <div className="hero relative">
-        <div className="flex flex-col justify-center items-center mt-52 text-center">
-          <div className="text-6xl font-bold text-[#273266]">
-            Effortless Events,<span className=" block">Maximum Engagement</span>{' '}
-          </div>
-          <div className="w-8/12 font-bold text-[#273266] my-6 description">
-            Plan, manage, and optimize events with ease—powerful tools for seamless organization and
-            interactive experiences.
-          </div>
-        </div>
+interface HeroProps {
+  servicesRef: React.RefObject<HTMLDivElement | null>;
+}
 
-        <div className="join flex gap-2 z-50">
-          <Link to={FrontEndRoutes.SignUp} className="text-[#273266] font-bold">
-            <CustomButton hoverColor="hover:bg-[#3b4edb]">Get Started</CustomButton>
-          </Link>
+export default function Hero({ servicesRef }: HeroProps) {
+  const scrollToServices = () => {
+    servicesRef?.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <div className="hero relative">
+      <div className="flex flex-col justify-center items-center mt-52 text-center">
+        <div className="text-6xl font-bold text-[#273266]">
+          Effortless Events,<span className=" block">Maximum Engagement</span>
+        </div>
+        <div className="w-8/12 font-bold text-[#273266] my-6 description">
+          Plan, manage, and optimize events with ease—powerful tools for seamless organization and
+          interactive experiences.
+        </div>
+      </div>
+
+      <div className="join flex gap-2 z-20">
+        <Link to={FrontEndRoutes.SignUp} className="text-[#273266] font-bold">
+          <CustomButton hoverColor="hover:bg-[#3b4edb]">Get Started</CustomButton>
+        </Link>
+        <button onClick={scrollToServices}>
           <CustomButton bgColor="bg-gray-200" textColor="text-[#1E2A78] hover:bg-gray-300">
             Learn More
           </CustomButton>
-        </div>
-        <img src={heroShape} alt="Hero Shape" className="z-0 absolute bottom-0 inset-x-0 mx-auto" />
+        </button>
       </div>
-    </>
+      <img src={heroShape} alt="Hero Shape" className="z-0 absolute bottom-0 inset-x-0 mx-auto" />
+    </div>
   );
 }
