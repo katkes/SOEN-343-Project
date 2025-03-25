2d73088630c06be1849fbe1ff2267c9b87e4f7ba
Keshan Kathiripilay <ke_kathi@live.concordia.ca>
2025-03-24 21:09:09 -0400

    Minor documentation tweaks
    Tweak to API key definition along with removal of unnecessary controller

--- backend/src/controllers/payment.ts
+++
@@ -1,7 +0,0 @@
-// import { Request, Response } from 'express';
-// import { StripeFacade } from '../services/stripe/StripeFacade';
-
-// const stripeFacade = new StripeFacade();
-
-// TODO: To be implemented in future
-// export const purchaseTicket = async (req: Request, res: Response) => {};
