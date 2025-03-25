3a291b7463e514b0ef75c42e174dbd1dab1bcf85
Keshan Kathiripilay <ke_kathi@live.concordia.ca>
2025-03-24 20:58:54 -0400

    Stripe API Facade
    Addition and integration of Stripe API Facade, along with testing connection

---
+++ backend/src/controllers/payment.ts
@@ -0,0 +1,7 @@
+// import { Request, Response } from 'express';
+// import { StripeFacade } from '../services/stripe/StripeFacade';
+
+// const stripeFacade = new StripeFacade();
+
+// TODO: To be implemented in future
+// export const purchaseTicket = async (req: Request, res: Response) => {};
