8bfd0cbdea70d1c413ee9a7fa514baf2dbc03d8b
Keshan Kathiripilay <ke_kathi@live.concordia.ca>
2025-03-27 13:37:22 -0400

    Remove userFirstName userLastName

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -32,9 +32,9 @@ export const Profile: React.FC = () => {
         <Sidebar />
         <div className="flex-1 overflow-y-auto p-8">
           <div className="flex items-center justify-between mb-6">
-            <h1 className="text-4xl font-bold text-[#273266] bg-white px-6 py-2 rounded-full shadow">
+            {/* <h1 className="text-4xl font-bold text-[#273266] bg-white px-6 py-2 rounded-full shadow">
               {userFirstName} {userLastName}
-            </h1>
+            </h1> */}
             <div className="bg-[#273266] text-white py-2 px-4 rounded-xl font-medium text-sm">
               <p className="text-gray-500">{currentDate}</p>
             </div>
